d0f05208a111609021d6a73026b80bc2cbb23a3c
Derick Ostrenko <dostrenko@gmail.com>
2015-01-05 13:24:46 -0600

    added url

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,3 +23,5 @@ module.exports = function(grunt) {
 	//Default Tasks
 	grunt.registerTask('default', ['shell']);
 };
+
+//https://doc-0g-6c-docs.googleusercontent.com/docs/secure/4kdvpse1r50dj305fn9aacudpgv428ks/plr1q43p85fi0s5fd5r5vffj1kgnqbur/1420473600000/06702729442107668201/06702729442107668201/YuQHT0unAJNOWUd2ur1GfJDkNbRFv7KVCPEXZQ8ixuJiDBTIk9GV3FjtTjMW26L3Y_Aq7iFZtfFAIf3sBGO18M6mXXH3T-0898q0HXlHyNHdr9Ruo5gt2hdpXL6wa_eR?ai=YuQHT0unAJNOWUd2ur1GfJDkNbRFv7KVCPEXZQ8ixuJiDBTIk9GV3FjtTjMW26L3Y_Aq7iFZtfFAIf3sBGO18M6mXXH3T-0898q0HXlHyNHdr9Ruo5gt2hdpXL6wa_eR&ap=grOCTZLfQ5UvCL0ymS_mPP8eGXG9a7Lc0-q_BAkseQXW-m9dySAbQQ&a=Ready_to_go_-_Jason_and_Kristen-2015-01-05.zip&nonce=74li53hg4tfee&user=06702729442107668201&hash=6vqgia23ag3kv3deh9c110cpqamsk4h3
